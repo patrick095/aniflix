748635e45c40ba477efb6e6181bcd562d01afd7d
Patrick Chaves <patrick095@gmail.com>
2020-08-09 23:49:43 -0300

    responsive bug fixes

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const URL = window.location.hostname.includes('localhost')
-  ? 'http://localhost:3000'
+  ? 'http://localhost:3001'
   : 'https://aniflix-backend.herokuapp.com';
 // tive que corrigir um bug!!!
 export default {
